1e5b516d82a57d0e9346fdea37543c9310401a6a
merouane.business01 <merouane.business01@gmail.com>
2024-09-12 21:59:38 +0100

    feat: add the regisret schema to the schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -5,3 +5,8 @@ export const LoginSchema = z.object({
   email: z.string().email({ message: "Email is required" }),
   password: z.string().min(8, { message: "Password is required" }),
 });
+export const RegisterSchema = z.object({
+  email: z.string().email({ message: "Email is required" }),
+  password: z.string().min(8, { message: "Minimum 8 characters required" }),
+  name: z.string().min(5, { message: "Name is required" }),
+});
