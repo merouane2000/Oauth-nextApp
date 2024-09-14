8c0a41420512b6245b64e3ee33ac357d24d89fc6
merouane.business01 <merouane.business01@gmail.com>
2024-09-14 13:06:12 +0100

    feat : add the errors auth routes

--- routes.ts
+++ routes.ts
@@ -6,7 +6,8 @@ export const publicRoutes =[
 
 export const authRoutes =[
     "/auth/login",
-    "/auth/register"
+    "/auth/register",
+    "/auth/error"
 ]
 
 export const apiAuthPrefix ="/api/auth"
