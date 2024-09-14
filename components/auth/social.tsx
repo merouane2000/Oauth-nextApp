b30639d3da99dce54e4f76e9fb2e7ed1d3838593
merouane.business01 <merouane.business01@gmail.com>
2024-09-14 13:07:54 +0100

    feat: add functionalty to the social buttons

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,18 +1,27 @@
+"use client"
 import { FcGoogle } from "react-icons/fc";
 import {FaGithub } from "react-icons/fa";
 import { Button } from "../ui/button";
+import {signIn} from 'next-auth/react'
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 
 
 
 export const Social = () => {
+  const onClick=(provider: "google" | "github")=>{
+    signIn(provider,{
+      callbackUrl:DEFAULT_LOGIN_REDIRECT
+    })
+
+  }
   return (
     <div className=" flex w-full gap-x-2 items-center ">
      <Button 
      size='lg'
      variant='outline'
      className="w-full"
-    //  onClick={()=>{}}
+     onClick={()=>onClick("google")}
      >
      <FcGoogle className="h-5 w-5"/>   
      </Button>
@@ -20,7 +29,7 @@ export const Social = () => {
      size='lg'
      variant='outline'
      className="w-full"
-    //  onClick={()=>{}}
+     onClick={()=>onClick("github")}
      >
      <FaGithub className="h-5 w-5"/>   
      </Button>
