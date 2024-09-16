d5786cb5ed5d01c8e964153f2a4897bcd2ccbe74
merouane.business01 <merouane.business01@gmail.com>
2024-09-16 23:22:49 +0100

    fix : typeScript error

--- auth.ts
+++ auth.ts
@@ -4,7 +4,6 @@ import { db } from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { getUserById } from "./data/user";
 import { UserRole } from "@prisma/client";
-import { use } from "react";
 
 declare module "next-auth" {
   interface session {
@@ -41,9 +40,10 @@ export const {
 
       if( account?.provider !== "Credentials") return true ;
 
-      const existingUser = await getUserById(user.id);
+      const existingUser = await getUserById(user.id!);
 
       if(!existingUser?.emailVerified) return false;
+      
       return true;
     },
     async session({ token, session }) {
